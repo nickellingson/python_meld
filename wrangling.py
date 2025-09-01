35b5a6d65cc2f0c90540d48f1dffd0e82f4a8f07
Nicholas Ellingson <nickellingson8@gmail.com>
2025-09-01 13:45:24 -0500

    feat: pandas

--- wrangling.py
+++ wrangling.py
@@ -2,3 +2,18 @@ import pandas as pd
 
 employees_df = pd.read_csv('./test.csv', delimiter="\t")
 print(employees_df)
+
+# Clean salary column
+print(employees_df["Salary"])
+employees_df["Salary"] = employees_df["Salary"].str.replace(",", "")
+print(employees_df)
+employees_df["Salary"] = pd.to_numeric(employees_df["Salary"])
+
+print(employees_df.columns.tolist())
+
+# Average salary for each department
+avg_salary_df = employees_df.groupby("Department").agg(
+    avg_salary_by_dept = ("Salary", "mean"),
+    count_of_dept = ("Department", "count")
+)
+print(avg_salary_df)
